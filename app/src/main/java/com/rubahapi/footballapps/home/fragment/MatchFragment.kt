90a64ebfbfe6fcf50b7001fa0c706ac38eab8b5e
junifar <junifar@gmail.com>
2018-11-16 18:01:15 +0700

    Layout Beginner 2

--- app/src/main/java/com/rubahapi/footballapps/home/fragment/MatchFragment.kt
+++ app/src/main/java/com/rubahapi/footballapps/home/fragment/MatchFragment.kt
@@ -2,23 +2,50 @@ package com.rubahapi.footballapps.home.fragment
 
 import android.content.Context
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
+import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.rubahapi.footballapps.R
 import org.jetbrains.anko.*
-import org.jetbrains.anko.design.appBarLayout
 import org.jetbrains.anko.design.coordinatorLayout
+import org.jetbrains.anko.design.tabLayout
+import org.jetbrains.anko.design.themedAppBarLayout
 import org.jetbrains.anko.support.v4.ctx
 
 class MatchFragment: Fragment(), AnkoComponent<Context>{
 
+    lateinit var layoutTab: TabLayout
+    lateinit var appBarLayout: AppBarLayout
+
     override fun createView(ui: AnkoContext<Context>): View {
+//        val tab = layoutTab.newTab()
+//        tab.text = "Test"
+//        layoutTab.addTab(tab)
         return setupUI(ui)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return createView(AnkoContext.create(ctx))
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val tab = layoutTab.newTab()
+        tab.text = "Test"
+
+        val tab2 = layoutTab.newTab()
+        tab2.text = "Test2"
+
+        val tab3 = layoutTab.newTab()
+        tab3.text = "Test3"
+
+        layoutTab.addTab(tab)
+        layoutTab.addTab(tab2)
+        layoutTab.addTab(tab3)
     }
 
     private fun setupUI(ui:AnkoContext<Context>) = with(ui){
@@ -30,13 +57,14 @@ class MatchFragment: Fragment(), AnkoComponent<Context>{
 
             fitsSystemWindows = false
 
-            appBarLayout {
+            appBarLayout = themedAppBarLayout(R.style.AppTheme_AppBarOverlay){
                 lparams(
                     width = matchParent,
                     height = wrapContent
                 )
-//                topPadding = dip(8)
-                toolbar {
+
+                themedToolbar(R.style.AppTheme_PopupOverlay) {
+                    backgroundResource = R.color.colorPrimary
                     lparams(
                         width = matchParent,
                         height = android.R.attr.actionBarSize
@@ -44,11 +72,18 @@ class MatchFragment: Fragment(), AnkoComponent<Context>{
                 }.lparams{
                     weight = 1f
                 }
+
+                layoutTab = tabLayout {
+                    lparams(
+                        width = matchParent,
+                        height = wrapContent
+                    )
+                }
             }
 
-            textView {
-                text = "Match test"
-            }
+//            textView {
+//                text = "Match test"
+//            }
         }
     }
 }
