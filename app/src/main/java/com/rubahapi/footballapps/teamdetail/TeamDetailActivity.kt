aaeb4fdaf5bbfd92adf15bd113c6cdad7b32e69e
junifar <junifar@gmail.com>
2018-11-23 18:06:49 +0700

    Team Fragment detail on progress

--- app/src/main/java/com/rubahapi/footballapps/teamdetail/TeamDetailActivity.kt
+++ app/src/main/java/com/rubahapi/footballapps/teamdetail/TeamDetailActivity.kt
@@ -1,38 +1,95 @@
 package com.rubahapi.footballapps.teamdetail
 
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout.ScrollingViewBehavior
+import android.support.design.widget.CollapsingToolbarLayout
 import android.support.v7.app.AppCompatActivity
+import com.rubahapi.footballapps.R
 import com.rubahapi.footballapps.models.Team
-import org.jetbrains.anko.*
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_team_detail.*
+import org.jetbrains.anko.colorAttr
+import org.jetbrains.anko.design.collapsingToolbarLayout
+import org.jetbrains.anko.design.coordinatorLayout
+import org.jetbrains.anko.design.themedAppBarLayout
+import org.jetbrains.anko.dip
+import org.jetbrains.anko.matchParent
+import org.jetbrains.anko.textView
 
 class TeamDetailActivity: AppCompatActivity(){
 
     lateinit var item:Team
+    lateinit var collapsingToolbarLayout: CollapsingToolbarLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team_detail)
+        setSupportActionBar(toolbar)
+
         item = intent.getParcelableExtra("item")
 
-        setupUI()
+        Picasso.get().load(item.teamBadge).fit().into(imgThumb)
+        str_team.text = item.teamName
+        int_formed_year.text = item.intFormedYear.toString()
+        str_stadium.text = item.strStadium
+
+//        setupUI()
+//        setConfiguration()
+    }
+
+    private fun setConfiguration(){
+
     }
 
     private fun setupUI(){
-        scrollView {
-            lparams{
+        coordinatorLayout {
+            lparams {
                 width = matchParent
                 height = matchParent
+                behavior = ScrollingViewBehavior()
             }
+            fitsSystemWindows = true
 
-            verticalLayout {
-                lparams{
+            themedAppBarLayout(R.style.AppTheme_AppBarOverlay) {
+                fitsSystemWindows = true
+                lparams {
                     width = matchParent
-                    height = wrapContent
+                    height = dip(180)
                 }
 
-                textView {
-                    text = "coba"
+                collapsingToolbarLayout = collapsingToolbarLayout {
+                    fitsSystemWindows = true
+                    lparams {
+                        width = matchParent
+                        height = matchParent
+                    }
+                    contentScrim = ColorDrawable(colorAttr(R.attr.colorPrimary))
+                  textView {
+                        text = "coba"
+                    }
                 }
+            }
+            textView {
+                text = "coba"
             }
         }
+//        scrollView {
+//            lparams{
+//                width = matchParent
+//                height = matchParent
+//            }
+//
+//            verticalLayout {
+//                lparams{
+//                    width = matchParent
+//                    height = wrapContent
+//                }
+//
+//                textView {
+//                    text = "coba"
+//                }
+//            }
+//        }
     }
 }
